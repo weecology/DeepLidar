48e82efa588ca12ae5d8d161a957717c34771904
bw4sz <benweinstein2010@gmail.com>
2019-05-22 13:00:16 -0700

    Still getting dask loss? didn't happen with no normalize

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -17,7 +17,6 @@ DeepForest_config = config.load_config(dir="..")
 site = "TEAK"
 tile_xml = "../data/TEAK/annotations/2018_TEAK_3_315000_4094000_image_crop.xml"
 
-@profile(precision=precision, stream=fp)
 def test_Generate_xml(tile_xml, DeepForest_config, site):
     #Create generate
     csv_file, h5_file = Generate.run(tile_xml, DeepForest_config, site)
@@ -31,6 +30,7 @@ def test_Generate_xml(tile_xml, DeepForest_config, site):
 #test_Generate_xml(data, DeepForest_config,site)
 
 tile_csv = "/Users/ben/Documents/DeepLidar/data/TEAK/training/NEON_D17_TEAK_DP1_324000_4104000_classified_point_cloud_colorized.csv"
+@profile(precision=precision, stream=fp)
 def test_Generate_csv(tile_csv, DeepForest_config, site):
     csv_file, h5_file  = Generate.run(tile_csv, DeepForest_config=DeepForest_config, site=site)
 
