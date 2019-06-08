332d69086c12a5fe2d7c0ccb7ee2536426e671b0
bw4sz <benweinstein2010@gmail.com>
2019-06-07 18:42:23 -0700

    window

--- DeepForest/onthefly_generator.py
+++ DeepForest/onthefly_generator.py
@@ -128,7 +128,7 @@ class OnTheFlyGenerator(Generator):
         
         #Sanity check
         if len(classes) > 1:
-            print(self.windowdf[self.windowdf.label!="Tree"])
+            print(self.data[self.data.label!="Tree"])
             print("There are too many classes")
             raise ValueError
         
