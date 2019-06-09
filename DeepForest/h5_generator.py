59251e03982a61dfc6d663c6aae60af2ffe42f54
bw4sz <benweinstein2010@gmail.com>
2019-06-08 20:07:44 -0700

    Still sorting by site

--- DeepForest/h5_generator.py
+++ DeepForest/h5_generator.py
@@ -64,7 +64,7 @@ class H5Generator(Generator):
             self.labels[value] = key        
         
         #Set groups at first order.
-        self.define_groups(shuffle=False)
+        self.define_groups(shuffle=True)
         
         #report total number of annotations
         self.total_trees = self.total_annotations()
