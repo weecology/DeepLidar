d079cdc84bf659c8546d87965a6402d60bdbc203
bw4sz <benweinstein2010@gmail.com>
2019-06-09 20:23:20 -0700

    what to do if postprocessing deletes all predictions

--- DeepForest/evalmAP.py
+++ DeepForest/evalmAP.py
@@ -136,7 +136,6 @@ def _get_detections(generator, model, score_threshold=0.05, max_detections=300, 
                                 
                 if postprocess:
                     #find window utm coordinates
-                    print("Postprocessing {} predictions".format(len(image_boxes)))
                     #print("Bounds for image {}, window {}, are {}".format(generator.row["tile"], generator.row["window"], bounds))
                     pc = postprocessing.drape_boxes(boxes=image_boxes, pc = generator.lidar_tile, bounds=bounds)     
                     
@@ -144,7 +143,11 @@ def _get_detections(generator, model, score_threshold=0.05, max_detections=300, 
                     image_boxes = postprocessing.cloud_to_box(pc, bounds)    
                     image_scores = image_scores[:image_boxes.shape[0]]
                     image_labels = image_labels[:image_boxes.shape[0]] 
-                    image_detections = np.concatenate([image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_labels, axis=1)], axis=1)
+                    if len(image_boxes)>0:
+                        image_detections = np.concatenate([image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_labels, axis=1)], axis=1)
+                    else:
+                        pass
+                    #TODO what to do if there are no rows?
                 else:
                     pass
                     #print("Point density of {:.2f} is too low, skipping image {}".format(density, generator.row["tile"]))        
