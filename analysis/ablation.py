d0b9a4e5a42a820774619f847f0d11bfc475dc88
bw4sz <benweinstein2010@gmail.com>
2019-07-18 13:44:58 -0700

    trying a different strategy

--- analysis/ablation.py
+++ analysis/ablation.py
@@ -37,133 +37,139 @@ def get_session():
 
 keras.backend.tensorflow_backend.set_session(get_session())
 
-#The following models have been pretrained on all other sites except for the name in the site key
-pretraining_models = {
-    "SJER": "/orange/ewhite/b.weinstein/retinanet/20190711_121755/resnet50_05.h5",
-    "TEAK": "/orange/ewhite/b.weinstein/retinanet/20190713_102002/resnet50_04.h5",
-    "NIWO":"/orange/ewhite/b.weinstein/retinanet/20190711_195718/resnet50_05.h5",
-    "MLBS":  "/orange/ewhite/b.weinstein/retinanet/20190711_121214/resnet50_05.h5"
-}
+def train(pretrain_model_path, data, proportion_data, DeepForest_config):
+    ###Log experiments
+    experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar', log_code=False)
+    
+    #make snapshot dir            
+    dirname = datetime.now().strftime("%Y%m%d_%H%M%S")  
+    experiment.log_parameter("Start Time", dirname)                
+    save_snapshot_path=DeepForest_config["save_snapshot_path"] + dirname            
+    os.mkdir(save_snapshot_path)   
+    
+    ##Replace config file and experiment
+    DeepForest_config["batch_size"] = 20
+    DeepForest_config["epochs"] = 2
+    DeepForest_config["save_image_path"] =  None
+    experiment.log_parameter("mode","ablation")   
+    DeepForest_config["evaluation_images"] =0         
+    
+    #set training images, as a function of the number of training windows
+    DeepForest_config["training_proportion"] = proportion_data     
+    experiment.log_parameters(DeepForest_config)                
 
-#For each site, match the hand annotations with the pretraining model
-results = []
-for pretraining_site in pretraining_models:  
-    
-    pretrain_model_path = pretraining_models[pretraining_site]
-    print("Running pretraining for {}".format(pretraining_site))
-    
-    #Load retraining data
-    DeepForest_config["hand_annotation_site"] = [pretraining_site]
-    DeepForest_config["evaluation_site"] = [pretraining_site]
-    
-    data = load_retraining_data(DeepForest_config)
-    for x in [pretraining_site]:
-        DeepForest_config[x]["h5"] = os.path.join(DeepForest_config[x]["h5"],"hand_annotations")
-        print(DeepForest_config[x]["h5"])
+    if not proportion_data == 0:
+        #Run training, and pass comet experiment class
+        #start training
+        train_generator, validation_generator = create_h5_generators(data, DeepForest_config=DeepForest_config)     
         
-    #Load pretraining model
-    print('Loading model, this may take a secondkeras-retinanet.\n')
+        model, training_model, prediction_model = create_models(
+            backbone_retinanet=backbone.retinanet,
+                        num_classes=1,
+                       weights=pretrain_model_path,
+                       multi_gpu=DeepForest_config["num_GPUs"],
+                       freeze_backbone=False,
+                       nms_threshold=DeepForest_config["nms_threshold"],
+                       input_channels=DeepForest_config["input_channels"]
+        )
+        
+        #create callback, a bit annoying to keep the retinanet machinery intact
+        callbacks = []
+        checkpoint = keras.callbacks.ModelCheckpoint(
+            os.path.join(
+                save_snapshot_path,
+                '{backbone}_{{epoch:02d}}.h5'.format(backbone=DeepForest_config["backbone"])
+            ),
+            verbose=1,
+        period=10)
+        
+        checkpoint = RedirectModel(checkpoint, model)
+        callbacks.append(checkpoint)
+        
+        #ensure directory created first; otherwise h5py will error after epoch.
+        history = training_model.fit_generator(
+            generator=train_generator,
+            steps_per_epoch=train_generator.size()/DeepForest_config["batch_size"],
+            epochs=DeepForest_config["epochs"],
+            verbose=2,
+            shuffle=False,
+            callbacks=callbacks,
+            workers=DeepForest_config["workers"],
+            use_multiprocessing=DeepForest_config["use_multiprocessing"],
+            max_queue_size=DeepForest_config["max_queue_size"])
+        
+        num_trees = train_generator.total_trees
+        experiment.log_parameter("Number of Training Trees", num_trees)   
+        
+        #return path snapshot of final epoch
+        saved_models = glob.glob(os.path.join(save_snapshot_path,"*.h5"))
+        saved_models.sort()
+        trained_model_path = saved_models[-1]
+    else: 
+        # load the model just once
+        print('Loading model, this may take a second...')
+        trained_model_path = pretrain_model_path
+        num_trees = 0
     
-    backbone = models.backbone(DeepForest_config["backbone"])         
+    experiment.log_parameter("Number of Training Trees", num_trees)           
+    return trained_model_path
+
+def evaluation(trained_model_path, results, DeepForest_config):
+    #Run eval
+    experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar', log_code=False)
+    experiment.log_parameter("mode","ablation_evaluation")
+    experiment.log_parameters(DeepForest_config)            
+        
+    args = [
+        "--batch-size", str(DeepForest_config['batch_size']),
+        '--score-threshold', str(DeepForest_config['score_threshold']),
+        '--suppression-threshold', '0.1', 
+        '--save-path', 'snapshots/images/', 
+    ]
     
-    #For each site run a portion of the training data
-    for x in np.arange(2):
-        for proportion_data in [0, 0.01, 0.05,0.25,0.5,0.75,1]:
-            
-            ###Log experiments
-            experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar', log_code=False)
-            
-            #make snapshot dir            
-            dirname = datetime.now().strftime("%Y%m%d_%H%M%S")  
-            experiment.log_parameter("Start Time", dirname)                
-            save_snapshot_path=DeepForest_config["save_snapshot_path"] + dirname            
-            os.mkdir(save_snapshot_path)   
-            
-            ##Replace config file and experiment
-            DeepForest_config["batch_size"] = 30
-            DeepForest_config["epochs"] = 30
-            DeepForest_config["save_image_path"] =  None
-            experiment.log_parameter("mode","ablation")   
-            DeepForest_config["evaluation_images"] =0         
-            
-            #set training images, as a function of the number of training windows
-            DeepForest_config["training_proportion"] = proportion_data     
-            experiment.log_parameters(DeepForest_config)                
+    eval_model = models.load_model(trained_model_path, backbone_name="resnet50", convert=True, nms_threshold=DeepForest_config["nms_threshold"])                 
+    recall, precision  = eval_main(DeepForest_config = DeepForest_config, args = args, model=eval_model)
+    results.append({"Number of Trees": num_trees, "Proportion":proportion_data,"Evaluation Site" : pretraining_site, "Recall": recall,"Precision": precision})
+    
+    return results
 
-            if not proportion_data == 0:
-                #Run training, and pass comet experiment class
-                #start training
-                train_generator, validation_generator = create_h5_generators(data, DeepForest_config=DeepForest_config)     
-                
-                model, training_model, prediction_model = create_models(
-                    backbone_retinanet=backbone.retinanet,
-                                num_classes=1,
-                               weights=pretrain_model_path,
-                               multi_gpu=DeepForest_config["num_GPUs"],
-                               freeze_backbone=False,
-                               nms_threshold=DeepForest_config["nms_threshold"],
-                               input_channels=DeepForest_config["input_channels"]
-                )
-                
-                #create callback, a bit annoying to keep the retinanet machinery intact
-                callbacks = []
-                checkpoint = keras.callbacks.ModelCheckpoint(
-                    os.path.join(
-                        save_snapshot_path,
-                        '{backbone}_{{epoch:02d}}.h5'.format(backbone=DeepForest_config["backbone"])
-                    ),
-                    verbose=1,
-                period=10)
-                
-                checkpoint = RedirectModel(checkpoint, model)
-                callbacks.append(checkpoint)
-                
-                #ensure directory created first; otherwise h5py will error after epoch.
-                history = training_model.fit_generator(
-                    generator=train_generator,
-                    steps_per_epoch=train_generator.size()/DeepForest_config["batch_size"],
-                    epochs=DeepForest_config["epochs"],
-                    verbose=2,
-                    shuffle=False,
-                    callbacks=callbacks,
-                    workers=DeepForest_config["workers"],
-                    use_multiprocessing=DeepForest_config["use_multiprocessing"],
-                    max_queue_size=DeepForest_config["max_queue_size"])
-                
-                num_trees = train_generator.total_trees
-                
-                #return path snapshot of final epoch
-                saved_models = glob.glob(os.path.join(save_snapshot_path,"*.h5"))
-                saved_models.sort()
-                print(saved_models)
-                trained_model_path = saved_models[-1]
-
-            else: 
-                # load the model just once
-                print('Loading model, this may take a second...')
-                trained_model_path = pretrain_model_path
-                num_trees = 0
+if __name__ == "__main__":
+    #The following models have been pretrained on all other sites except for the name in the site key
+    pretraining_models = {
+        "SJER": "/orange/ewhite/b.weinstein/retinanet/20190711_121755/resnet50_05.h5",
+        "TEAK": "/orange/ewhite/b.weinstein/retinanet/20190713_102002/resnet50_04.h5",
+        "NIWO":"/orange/ewhite/b.weinstein/retinanet/20190711_195718/resnet50_05.h5",
+        "MLBS":  "/orange/ewhite/b.weinstein/retinanet/20190711_121214/resnet50_05.h5"
+    }
+    
+    #For each site, match the hand annotations with the pretraining model
+    results = []
+    for pretraining_site in pretraining_models:  
+        
+        pretrain_model_path = pretraining_models[pretraining_site]
+        print("Running pretraining for {}".format(pretraining_site))
+        
+        #Load retraining data
+        DeepForest_config["hand_annotation_site"] = [pretraining_site]
+        DeepForest_config["evaluation_site"] = [pretraining_site]
+        
+        data = load_retraining_data(DeepForest_config)
+        for x in [pretraining_site]:
+            DeepForest_config[x]["h5"] = os.path.join(DeepForest_config[x]["h5"],"hand_annotations")
+            print(DeepForest_config[x]["h5"])
             
-            experiment.log_parameter("Number of Training Trees", num_trees)   
-                
-            #Run eval
-            experiment = Experiment(api_key="ypQZhYfs3nSyKzOfz13iuJpj2", project_name='deeplidar', log_code=False)
-            experiment.log_parameter("mode","ablation_evaluation")
-            experiment.log_parameters(DeepForest_config)            
-                
-            args = [
-                "--batch-size", str(DeepForest_config['batch_size']),
-                '--score-threshold', str(DeepForest_config['score_threshold']),
-                '--suppression-threshold', '0.1', 
-                '--save-path', 'snapshots/images/', 
-            ]
-            
-            eval_model = models.load_model(trained_model_path, backbone_name="resnet50", convert=True, nms_threshold=DeepForest_config["nms_threshold"])                 
-            recall, precision  = eval_main(DeepForest_config = DeepForest_config, args = args, model=eval_model)
-            results.append({"Number of Trees": num_trees, "Proportion":proportion_data,"Evaluation Site" : pretraining_site, "Recall": recall,"Precision": precision})
-            del(eval_model)
-            del(training_model)
-            gc.collect()
-results = pd.DataFrame(results)
-
-results.to_csv("ablation_{}".format(dirname) + ".csv")        
+        #Load pretraining model
+        print('Loading model, this may take a secondkeras-retinanet.\n')
+        
+        backbone = models.backbone(DeepForest_config["backbone"])         
+        
+        #For each site run a portion of the training data
+        for x in np.arange(2):
+            for proportion_data in [0, 0.01, 0.05,0.25,0.5,0.75,1]:
+                trained_model_path = train(pretrain_model_path, data, proportion_data, DeepForest_config)
+                results = evaluation(trained_model_path, results, DeepForest_config)
+    
+    #Wrap together the results            
+    results = pd.DataFrame(results)
+    
+    results.to_csv("ablation_{}".format(dirname) + ".csv")        
