59ff9253a2e8563336e682cd1764a4e62f116429
bw4sz <benweinstein2010@gmail.com>
2019-07-21 06:25:41 -0700

    dont' save images

--- analysis/prcurve.py
+++ analysis/prcurve.py
@@ -33,6 +33,8 @@ def main(DeepForest_config, model=None, args=None):
     dirname = datetime.now().strftime("%Y%m%d_%H%M%S")
     if args.save_path:
         save_path =  args.save_path + dirname
+    else:
+        save_path = None
         
         # make save path if it doesn't exist
         if not os.path.exists(save_path):
